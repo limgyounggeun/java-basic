d45520305a57a02527596e4cef584ec45edde565
limgyounggeun <choho4843@gmail.com>
2025-05-31 00:17:54 +0900

    [Feat] 패키지

---
+++ src/acess/Speaker.java
@@ -0,0 +1,25 @@
+package acess;
+
+public class Speaker {
+    private int volume;
+
+    Speaker(int volume){
+        this.volume = volume;
+    }
+    void volumeUp(){
+        if(volume >= 100){
+            System.out.println("음량을 증가할 수 없습니다. 최대 음량입니다.");
+        }else{
+            volume += 10;
+            System.out.println("음량을 10 증가 합니다.");
+        }
+    }
+
+    void volumeDown(){
+        volume -= 10;
+        System.out.println("volumeDown호출");
+    }
+    void showVolume(){
+        System.out.println("현재 음량 "+volume);
+    }
+}
